0c442d2c87f8fdab3b7e34cf30e74fb266f8d51f
Silve27 <k.prp0127@gmail.com>
2023-12-12 11:57:12 +0700

    jar file added.

--- src/battleEntity/monster/GrandMasterWarlock.java
+++ src/battleEntity/monster/GrandMasterWarlock.java
@@ -8,7 +8,7 @@ import javafx.scene.image.WritableImage;
 public class GrandMasterWarlock extends BaseUnit {
     public GrandMasterWarlock(String name,int baseHp,int baseMp, int baseAttack,int baseDefense){
         super(name,baseHp,baseMp,baseAttack,baseDefense);
-        Image image = new Image("/monster/GrandMasterWarlock.png");
+        Image image = new Image("/monster/GrandmasterWarlock.png");
         images = new WritableImage[9];
         images[0] = new WritableImage(image.getPixelReader(),0,0,128,128);
         images[1] = new WritableImage(image.getPixelReader(),128,0,128,128);
--- src/worldObject/buildings/Statue.java
+++ src/worldObject/buildings/Statue.java
@@ -5,7 +5,7 @@ import javafx.scene.image.WritableImage;
 import worldObject.BaseObject;
 
 public class Statue extends BaseObject {
-    Image i = new Image("/object/Statue.png");
+    Image i = new Image("/object/statue.png");
     // x = 11 y = 239 x2 = 290  y2 = 433
     WritableImage image = new WritableImage(i.getPixelReader(),1,96,47,96);
     public Statue(double WorldX,double WorldY,double scaleX,double scaleY,double solidX,double solidY,double solidW,double solidH){
